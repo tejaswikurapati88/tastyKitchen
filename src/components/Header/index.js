27bfca694643cb6ec8e30d877a6364f5f8d33fff
Tejaswi Kurapati <tejaswikurapati88@gmail.com>
2024-11-04 11:15:27 +0000

    updated to mobile view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import {withRouter, Link} from 'react-router-dom'
 import './index.css'
 import Cookies from 'js-cookie'
+import {GiHamburger} from 'react-icons/gi'
+import {useState} from 'react'
 
 const Header = props => {
+  const [isHam, setHam] = useState(false)
   const onLogout = () => {
     const {history} = props
     Cookies.remove('jwt_token')
     history.replace('/login')
+  }
+  const onham = () => {
+    setHam(!isHam)
+    console.log(isHam)
   }
   return (
     <div className="bg-container-header">
@@ -21,6 +28,24 @@ const Header = props => {
         </div>
       </Link>
 
+      <GiHamburger onClick={onham} className="hamburger" />
+      {isHam === true && (
+        <ul className="buttonshame">
+          <li to="/">
+            <Link to="/" className="header-butn">
+              Home
+            </Link>
+          </li>
+          <li to="/Cart">
+            <Link to="/cart" className="header-butn">
+              Cart
+            </Link>
+          </li>
+          <button type="button" onClick={onLogout} className="logout-btn">
+            Logout
+          </button>
+        </ul>
+      )}
       <ul className="buttons">
         <li to="/">
           <Link to="/" className="header-butn">
